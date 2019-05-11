bbf5a48d61f026ce478dc640cc2c119151206a79
Philipp Rabe <philipp.rabe@kolibrigames.com>
2019-05-11 12:27:32 +0200

    Added an action and reward logic

--- Assets/AI/CrabAgent.cs
+++ Assets/AI/CrabAgent.cs
@@ -6,6 +6,8 @@ public class CrabAgent : Agent
    [SerializeField] private Rigidbody2D _rigidbody;
    [SerializeField] private Rigidbody2D _ball;
 
+   private float _lastActionTime;
+   
    public override void AgentReset()
    {
       _rigidbody.velocity = Vector2.zero;
@@ -20,5 +22,14 @@ public class CrabAgent : Agent
       
       AddVectorObs(_ball.position);
       AddVectorObs(_ball.velocity);
+   }
+
+   public override void AgentAction(float[] vectorAction, string textAction)
+   {
+      transform.position += new Vector3(vectorAction[0], 0, 0);
+
+      var sinceLastAction = Time.time - _lastActionTime;
+      _lastActionTime = Time.time;
+      AddReward(sinceLastAction);
    }
 }
