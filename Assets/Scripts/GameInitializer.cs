f96464518a50b74006a81af5d384e68e693b436d
Jeremy <jeremy.ries@kolibrigames.com>
2019-05-11 16:49:52 +0200

    new scene for 1vs1

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -8,19 +8,20 @@ public class GameInitializer : MonoBehaviour
     [SerializeField] private GameObject _gameSetup;
     [SerializeField] private int _amount;
     [SerializeField] private Camera _camera;
-    
+
+    private float _size = 30f;
     void Start()
     {
         for (int i = 0; i < _amount; i++)
         {
             for (int j = 0; j < _amount; j++)
             {
-                Instantiate(_gameSetup, new Vector3(15 * i, 15 * j, 0), Quaternion.identity);
+                Instantiate(_gameSetup, new Vector3(_size * i, _size * j, 0), Quaternion.identity);
             }
         }
 
-        var bla = _amount / 2f * 15 - 7.5f;
+        var bla = _amount / 2f * _size - _size/2;
         _camera.transform.position = new Vector3(bla, bla, -8);
-        _camera.orthographicSize = _amount * 7.5f;
+        _camera.orthographicSize = _amount * _size/2;
     }
 }
