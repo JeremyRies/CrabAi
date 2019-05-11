027df7d0e8b23eed961838cc37d399a0a340e7ad
Philipp Rabe <philipp.rabe@kolibrigames.com>
2019-05-11 12:13:35 +0200

    Resetting the Agent to a random position

--- Assets/AI/CrabAgent.cs
+++ Assets/AI/CrabAgent.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using MLAgents;
+﻿using MLAgents;
 using UnityEngine;
 
 public class CrabAgent : Agent
 {
-   
+   [SerializeField] private Rigidbody2D _rigidbody;
+
+   public override void AgentReset()
+   {
+      _rigidbody.velocity = Vector2.zero;
+
+      var x = Random.Range(-4, -2);
+      transform.position = new Vector3(x, transform.position.y, transform.position.z);
+   }
 }
