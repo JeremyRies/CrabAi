316a3c0f01a0d073cb7c717aa07439127b957a83
Philipp Rabe <philipp.rabe@kolibrigames.com>
2019-05-11 12:17:18 +0200

    Added the collection of information

--- Assets/AI/CrabAgent.cs
+++ Assets/AI/CrabAgent.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class CrabAgent : Agent
 {
    [SerializeField] private Rigidbody2D _rigidbody;
+   [SerializeField] private Rigidbody2D _ball;
 
    public override void AgentReset()
    {
@@ -11,5 +12,13 @@ public class CrabAgent : Agent
 
       var x = Random.Range(-4, -2);
       transform.position = new Vector3(x, transform.position.y, transform.position.z);
+   }
+
+   public override void CollectObservations()
+   {
+      AddVectorObs(transform.position);
+      
+      AddVectorObs(_ball.position);
+      AddVectorObs(_ball.velocity);
    }
 }
